7e9e1e1fc981506aedaf26f7aaba92ec8a1bcac7
David LACOURT <david.lacourt@gmail.com>
2020-06-27 21:45:20 +0200

    Add dist

---
+++ dist/rollup-plugin-istanbul.cjs.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var rollupPluginutils = require('rollup-pluginutils');
+var istanbul = _interopDefault(require('istanbul-lib-instrument'));
+var path = require('path');
+
+function makeFilter(opts, extensions) {
+  var filter = rollupPluginutils.createFilter(opts.include, opts.exclude);
+
+  extensions = opts.extensions || extensions;
+  if (!extensions || extensions === '*') {
+    return filter;
+  }
+
+  if (!Array.isArray(extensions)) {
+    extensions = [extensions];
+  }
+  extensions = extensions.map(function (e) {
+    return e[0] !== '.' ? '.' + e : e;
+  });
+
+  return function (id) {
+    return filter(id) && extensions.indexOf(path.extname(id)) > -1;
+  };
+}
+
+function index () {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+  var filter = makeFilter(options, ['js', 'jsx', 'ts', 'tsx']),
+      opts = Object.assign({ esModules: true, compact: options.compact !== false }, options.instrumenterConfig, { produceSourceMap: options.sourceMap !== false }),
+      instrumenter = new (options.instrumenter || istanbul).createInstrumenter(opts);
+
+  return {
+    name: 'istanbul',
+    transform: function transform(code, id) {
+      if (!filter(id)) return;
+
+      // getCombinedSourceMap: https://github.com/rollup/rollup/issues/2983
+
+      var _getCombinedSourcemap = this.getCombinedSourcemap(),
+          version = _getCombinedSourcemap.version,
+          sources = _getCombinedSourcemap.sources,
+          sourcesContent = _getCombinedSourcemap.sourcesContent,
+          names = _getCombinedSourcemap.names,
+          mappings = _getCombinedSourcemap.mappings;
+
+      code = instrumenter.instrumentSync(code, id, { version: version, sources: sources, sourcesContent: sourcesContent, names: names, mappings: mappings });
+
+      return { code: code, map: instrumenter.lastSourceMap() };
+    }
+  };
+}
+
+module.exports = index;
